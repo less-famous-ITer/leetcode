1beaf378b06866352f75f999171056a12d3ddc1e
heyq <heyq020814@qq.com>
2023-03-21 20:52:56 +0800

    feat: 53.最大子数组和

---
+++ 学习计划/数据结构/第1天/53. 最大子数组和.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    function maxSubArray(nums: number[]): number {
+        const dp: number[] = [nums[0]];
+        for(let i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(
+                dp[i - 1] + nums[i],
+                nums[i]
+            );
+        }
+        return Math.max(...dp);
+    }
+
+})()
