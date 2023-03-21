5dac1906dcf7569b81f90bab4a5ed585972f5f4b
heyq <heyq020814@qq.com>
2023-03-21 20:52:38 +0800

    feat: 1137.第N个泰波那契数

---
+++ 学习计划/动态规划/第1天/1137. 第 N 个泰波那契数.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    function tribonacci(n: number): number {
+        const dp: number[] = [0, 1, 1];
+        for(let i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2]+ dp[i-3];
+        }
+        return dp[n];
+    }
+
+})()
